90ce729e3067e0e538efd6a29d1c26d024524d28
Matteo Baccan <matteo.baccan@gmail.com>
2019-07-26 09:08:21 +0200

    Correzione batch di compilazione

--- build190.bat
+++ build190.bat
@@ -1,13 +1,21 @@
 @echo off
 
+cls
+
 echo Imposta variabili d'ambiente
 echo per compilazione libreria Visual dBsee
 
+echo.
+echo Ricordarsi di impostare Alaska Xbase prima di lanciare questo .BAT
+echo.
+
+pause
+
 set VDBLIB_MAINDIR=%CD%\libreria
 set path=%VDBLIB_MAINDIR%\uti190;%path%
-set include=%include%;%VDBLIB_MAINDIR%\INCLUDE
-set include=%include%;%VDBLIB_MAINDIR%\SRC\EXTRA_CH
-set lib=%lib%;%VDBLIB_MAINDIR%\output\lib190\rel
+set include=%VDBLIB_MAINDIR%\INCLUDE;%include%
+set include=%VDBLIB_MAINDIR%\SRC\EXTRA_CH;%include%
+set lib=%VDBLIB_MAINDIR%\output\lib190\rel;%lib%
 
 echo Ambiente impostato correttamente su %VDBLIB_MAINDIR%
 
---
+++ build200-832.bat
@@ -0,0 +1,24 @@
+@echo off
+
+cls
+
+echo Imposta variabili d'ambiente
+echo per compilazione libreria Visual dBsee
+
+echo.
+echo Ricordarsi di impostare Alaska Xbase prima di lanciare questo .BAT
+echo.
+
+pause
+
+set VDBLIB_MAINDIR=%CD%\libreria
+set path=%VDBLIB_MAINDIR%\uti200;%path%
+set include=%VDBLIB_MAINDIR%\INCLUDE;%include%
+set include=%VDBLIB_MAINDIR%\SRC\EXTRA_CH;%include%
+set lib=%VDBLIB_MAINDIR%\output\lib200\rel;%lib%
+
+echo Ambiente impostato correttamente su %VDBLIB_MAINDIR%
+
+cd libreria\src\
+
+call gotutto200-832.bat
